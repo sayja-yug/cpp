f4573131ae1220db059b32d88623a4be0056623b
sayja-yug <yugsayja312@gmail.com>
2026-07-18 13:56:05 +0530

    lec 3

---
+++ lec-3/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int score  ;
+    score = 100;
+    const int uid = 232323;
+
+    int balance = 1000;
+
+    balance = 2000;
+
+
+
+    
+    cout <<"welcome to c++ programming  1"<< endl;
+    cout <<"welcome to c++ programming  2"<< endl;
+    cout <<"welcome to c++ programming  3"<< endl;
+
+    return 0;
+    
+}
